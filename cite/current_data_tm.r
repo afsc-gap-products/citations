bb5ab9099b644c69f8bb474045e410c8972eb957
emilymarkowitz-noaa <emily.markowitz@noaa.gov>
2024-10-13 12:16:41 -0700

    added crab data report to current_data_tm.r

--- cite/current_data_tm.r
+++ cite/current_data_tm.r
@@ -1,3 +1,3 @@
 srvy_cite <- data.frame(
-  SRVY = c("EBS", "NBS", "GOA", "AI", "BSS"),
-  CITE = c("2023NEBS", "2023NEBS","GOA2023", "AI2022", "RN979"))
+  SRVY = c("EBS", "NBS", "GOA", "AI", "BSS", "CRAB"),
+  CITE = c("2023NEBS", "2023NEBS","GOA2023", "AI2022", "RN979", "SAPcrab2024"))
