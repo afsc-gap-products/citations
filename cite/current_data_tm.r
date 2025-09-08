3441e67e751e255b1931c81c7a22c91115435ecd
Em Markowitz (NOAA) <75965120+EmilyMarkowitz-NOAA@users.noreply.github.com>
2025-09-08 08:42:47 -0700

    Update current_data_tm.r

--- cite/current_data_tm.r
+++ cite/current_data_tm.r
@@ -1,3 +1,3 @@
 srvy_cite <- data.frame(
   SRVY = c("EBS", "NBS", "GOA", "AI", "BSS", "CRAB"),
-  CITE = c("2023NEBS", "2023NEBS","GOA2023", "AI2022", "RN979", "SAPcrab2024"))
+  CITE = c("2024EBS", "2023NEBS","GOA2023", "AI2022", "RN979", "SAPcrab2024"))
